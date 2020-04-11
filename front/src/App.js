07178667f2a3e02e9af582510a97ec3bd07f5a72
Diego Riveros <df.riveros11@uniandes.edu.co>
2020-04-11 01:36:56 -0500

    done passport

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./layout/NavBar.js";
 import MainPage from "./layout/MainPage/MainPage.js";
 import MyFavors from "./layout/MyFavors/MyFavors.js";
@@ -7,24 +7,37 @@ import HelpSomeone from "./layout/HelpSomeone/HelpSomeone.js";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const App = () => (
-  <Router>
-    <NavBar></NavBar>
-    <Switch>
-      <Route exact path="/">
-        <MainPage />
-      </Route>
-      <Route exact path="/myfavors">
-        <MyFavors />
-      </Route>
-      <Route exact path="/askforfavor">
-        <AskForAFavor />
-      </Route>
-      <Route exact path="/helpsomeone">
-        <HelpSomeone />
-      </Route>
-    </Switch>
-  </Router>
-);
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/auth/getUser")
+      .then((res) => res.json())
+      .then((user) => {
+        console.log("APPUSER", user);
+        setUser(user);
+      });
+  }, []);
+
+  return (
+    <Router>
+      <NavBar user={user}></NavBar>
+      <Switch>
+        <Route exact path="/">
+          <MainPage user={user} />
+        </Route>
+        <Route exact path="/myfavors">
+          <MyFavors />
+        </Route>
+        <Route exact path="/askforfavor">
+          <AskForAFavor />
+        </Route>
+        <Route exact path="/helpsomeone">
+          <HelpSomeone />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
--- front/src/layout/MainPage/MainPage.js
+++ front/src/layout/MainPage/MainPage.js
@@ -2,17 +2,20 @@ import React from "react";
 import GoogleMap from "./../GoogleMap";
 import FormSignUPIN from "./FormSignUPIN.js";
 
-const MainPage = () => (
-  <div className="container">
-    <div className="row">
-      <div className="col-md-8">
-        <GoogleMap></GoogleMap>
-      </div>
-      <div className="col-6 col-md-4">
-        <FormSignUPIN></FormSignUPIN>
+const MainPage = (props) => {
+  console.log("MAINPAGEUSER", props.user);
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8">
+          <GoogleMap></GoogleMap>
+        </div>
+        <div className="col-6 col-md-4">
+          <FormSignUPIN user={props.user}></FormSignUPIN>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MainPage;
