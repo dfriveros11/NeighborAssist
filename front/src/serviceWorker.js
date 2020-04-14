db5397580f1d3fd353804e7adab29d9e3438d1e0
dartm05 <d.arteaga@uniandes.edu.co>
2020-04-14 08:34:18 -0500

    Update serviceWorker.js

--- front/src/serviceWorker.js
+++ front/src/serviceWorker.js
@@ -10,6 +10,8 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
+//Daniella Arteaga: Creo que esto no lo usan, tal vez es mejor mantener lo que utilizan.
+
 const isLocalhost = Boolean(
   window.location.hostname === "localhost" ||
     // [::1] is the IPv6 localhost address.
